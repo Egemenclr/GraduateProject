c8c7057fd9c3d20ca0f77c862f3dcd1a65488887
Egemen Inceler <45071098+Egemenclr@users.noreply.github.com>
2021-04-10 02:16:38 +0300

    Update Service.kt

--- Android/app/src/main/java/com/egemeninceler/donempro/Network/Service.kt
+++ Android/app/src/main/java/com/egemeninceler/donempro/Network/Service.kt
@@ -13,15 +13,15 @@ import retrofit2.http.Query
 
 interface CountryService {
 
-    @GET("api/city_prices?api_key=j81i915uscxvp3")
+    @GET("api/city_prices?api_key=APIKEY")
     fun getNumbeo(@Query("query") country: String, @Query("currency") currency: String ) : Call<Numbeo>
 
-    @GET("api/city_healthcare?api_key=j81i915uscxvp3&")
+    @GET("api/city_healthcare?api_key=APIKEY&")
     fun getHealtcare(@Query("query") city: String) : Call<Healtcare>
 
-    @GET("api/city_pollution?api_key=j81i915uscxvp3&")
+    @GET("api/city_pollution?api_key=APIKEY&")
     fun getPollution(@Query("query") city: String) : Call<Pollution>
 
-    @GET("api/city_crime?api_key=j81i915uscxvp3&")
+    @GET("api/city_crime?api_key=APIKEY&")
     fun getCrime(@Query("query") city: String) : Call<Crime>
 }
